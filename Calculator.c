8927e569c32e3171b5188882b7750eafac00988d
raffa <rem_1706@proton.me>
2025-08-20 11:22:07 +0200

    interface

--- Calculator.c
+++ Calculator.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-        printf("Hello, World!\n");
+    char mode[10];
+    printf("Version \x1b[31m1.2\x1b[0m\n");
+    printf("Welcome to the Calcinator 3000\n");
+    printf("\x1b[4mPlease choose an operation\x1b[0m:\n");
+    printf("\x1b[31mA = Addition\x1b[0m\n");
+    printf("\x1b[33ms = Subtraction\x1b[0m\n");
+    printf("\x1b[32mM = Multiplication\x1b[0m\n");
+    printf("\x1b[34mD = Division\x1b[0m\n");
+    printf("Enter your choice: ");
+    scanf("%s", mode);
+    printf("You chose: %s\n", mode);
     return 0;
-}   
+}
