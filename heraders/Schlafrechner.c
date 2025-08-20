ab04e6d398aae385c7fe70a00ca47fc05258001d
Hoang Vu <hoangvu.ho@noseryoung.com>
2025-08-20 15:42:54 +0200

    Schlafrechner

--- heraders/Schlafrechner.c
+++ heraders/Schlafrechner.c
@@ -2,7 +2,7 @@
 
 int main() {
     int wakeHour, wakeMinute;
-    int sleepHours;
+    float sleepHours;
 
     printf("Wann wachst du auf? (Stunde Minute)\n");
     if (scanf("%d %d", &wakeHour, &wakeMinute) != 2) {
@@ -10,21 +10,27 @@ int main() {
         return 1;
     }
 
-    printf("Wie viel willst du schlafen (in Stunden)?\n");
-    if (scanf("%d", &sleepHours) != 1) {
+    printf("Wie viel willst du schlafen (z. B. 8.5 Stunden)?\n");
+    if (scanf("%f", &sleepHours) != 1) {
         printf("Fehler bei der Eingabe!\n");
         return 1;
     }
 
-    int sleepHour = wakeHour - sleepHours;
-    int sleepMinute = wakeMinute;
 
-    if (sleepHour < 0) {
-        sleepHour += 24;
+    int totalSleepMinutes = (int)(sleepHours * 60);
+
+    int wakeTotalMinutes = wakeHour * 60 + wakeMinute;
+
+    int sleepTotalMinutes = wakeTotalMinutes - totalSleepMinutes;
+    if (sleepTotalMinutes < 0) {
+        sleepTotalMinutes += 24 * 60;
     }
 
+    int sleepHour = sleepTotalMinutes / 60;
+    int sleepMinute = sleepTotalMinutes % 60;
+
     printf("\nUm %02d:%02d solltest du einschlafen,", sleepHour, sleepMinute);
-    printf(" um %d Stunden zu schlafen.\n", sleepHours);
+    printf(" um %.2f Stunden zu schlafen.\n", sleepHours);
 
     return 0;
 }
