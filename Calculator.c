15e7f0ffaa44a3c1a8500b7909102673e1a25d5c
raffa <rem_1706@proton.me>
2025-08-20 16:36:54 +0200

    v-2.1.5_of_cal_simple

--- Calculator.c
+++ Calculator.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include "./headers/calc-f.h"
 
+
+
+
 int main() {
-    int a, b;
-    char op;
-    printf("Version \x1b[31m1.5\x1b[0m\n");
+    int a = 0, b = 0;
+    char op = 0;
+    char input[100];
+    printf("Version \x1b[31m2.1.5\x1b[0m\n");
     printf("Welcome to the Calcinator 3000\n");
     printf("\x1b[4mSupported Operations\x1b[0m:\n");
     printf("\x1b[31m+ = Addition\x1b[0m\n");
     printf("\x1b[33m- = Subtraction\x1b[0m\n");
     printf("\x1b[32m* = Multiplication\x1b[0m\n");
     printf("\x1b[34m/ = Division\x1b[0m\n");
+    printf("\x1b[35m^ = Exponentiation\x1b[0m\n");
+    printf("\x1b[36mr = Square Root\x1b[0m\n");
+    printf("\x1b[38m! = Factorial\x1b[0m\n");
     printf("Enter calculation:\n");
-    if (scanf("%d %c %d", &a, &op, &b) != 3) {
-        printf("Invalid input. Please use the format: number operator number\n");
+    fgets(input, sizeof(input), stdin);
+
+// removed stray character
+    if (sscanf(input, "%d %c %d", &a, &op, &b) == 3) {
+
+    } else if (sscanf(input, "%d%c", &a, &op) == 2 && op == 'r') {
+        b = 0;
+
+    } else if (sscanf(input, "%c%d", &op, &a) == 2 && op == 'r') {
+        b = 0;
+        // e.g. r4
+        } else if (sscanf(input, "%d%c", &a, &op) == 2 && op == '!') {
+            // Fakultät: e.g. 5!
+            printf("Factorial of %d is %llu\n", a, fakultaet(a));
+            return 0;
+    } else {
+        printf("Invalid input. Please use formats: number operator number, r<number>, <number>r\n");
         return 1;
     }
+
     switch (op) {
         case '+':
             add(a, b);
@@ -28,6 +51,12 @@ int main() {
             break;
         case '/':
             divide(a, b);
+            break;
+        case '^':
+            exponentiate(a, b);
+            break;
+        case 'r':
+            root(a);
             break;
         default:
             printf("Unsupported operation: %c\n", op);
--- headers/calc-f.h
+++ headers/calc-f.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void add(int a, int b) {
-    printf("Result: %.2f\n", a + b);
+    printf("Result: %d\n", a + b);
 }
 
 void subtract(int a, int b) {
@@ -9,7 +9,7 @@ void subtract(int a, int b) {
 }
 
 void multiply(int a, int b) {
-    printf("Result: %.2f\n", a * b);
+    printf("Result: %d\n", a * b);
 }
 
 void divide(int a, int b) {
@@ -18,4 +18,33 @@ void divide(int a, int b) {
     } else {
         printf("Error: Division by zero is not allowed.\n");
     }
+}
+void exponentiate(int base, int exp) {
+    int result = 1;
+    for (int i = 0; i < exp; i++) {
+        result *= base;
+    }
+    printf("Result: %d\n", result);
+}
+
+void root(int a) {
+    if (a < 0) {
+        printf("Error: Cannot calculate root of a negative number.\n");
+        return;
+    }
+    int res = 0;
+    while (res * res <= a) {
+        res++;
+    }
+    res--;
+    printf("Result: %d\n", res);
+}
+
+unsigned long long fakultaet(int n) {
+    if (n < 0) return 0; 
+    unsigned long long result = 1;
+    for (int i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
