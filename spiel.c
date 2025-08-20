79d8a5a5e4f45fe9aca7f2df41bd6eb10f2e5b0a
Ercan Bulgan <ercan.bulgan@noseryoung.com>
2025-08-20 16:46:19 +0200

    rechner mit extra spiel

---
+++ spiel.c
@@ -0,0 +1,12 @@
+//
+// Created by bulga on 20.08.2025.
+//
+
+#include "./spiel.h"
+
+int main() {
+    printf("Willkommen zu Scheren-Stein-Papier!\n");
+    spielStarten();
+    printf("Danke fürs Spielen!\n");
+    return 0;
+}
