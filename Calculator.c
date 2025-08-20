7fa544ccfe598d5566ed7a30db8de4208dbcb43b
raffa <rem_1706@proton.me>
2025-08-20 11:48:14 +0200

    dev

--- Calculator.c
+++ Calculator.c
@@ -2,14 +2,14 @@
 
 int main() {
     char mode[10];
-    printf("Version \x1b[31m1.2\x1b[0m\n");
+    printf("Version \x1b[31m1.5\x1b[0m\n");
     printf("Welcome to the Calcinator 3000\n");
-    printf("\x1b[4mPlease choose an operation\x1b[0m:\n");
-    printf("\x1b[31mA = Addition\x1b[0m\n");
-    printf("\x1b[33ms = Subtraction\x1b[0m\n");
-    printf("\x1b[32mM = Multiplication\x1b[0m\n");
-    printf("\x1b[34mD = Division\x1b[0m\n");
-    printf("Enter your choice: ");
+    printf("\x1b[4mSupported Operations\x1b[0m:\n");
+    printf("\x1b[31m+ = Addition\x1b[0m\n");
+    printf("\x1b[33m- = Subtraction\x1b[0m\n");
+    printf("\x1b[32m* = Multiplication\x1b[0m\n");
+    printf("\x1b[34m/ = Division\x1b[0m\n");
+    printf("Enter your calculation:\n");
     scanf("%s", mode);
     printf("You chose: %s\n", mode);
     return 0;
