81b6a8804459dfb6b123d3abcea82b9f2459773f
raffa <rem_1706@proton.me>
2025-08-21 10:33:00 +0200

    More bugfixes

--- Calculator.c
+++ Calculator.c
@@ -8,7 +8,7 @@ int main() {
     int a = 0, b = 0;
     char op = 0;
     char input[256];
-    printf("Version \x1b[31m2.1.9\x1b[0m\n");
+    printf("Version \x1b[31m2.2.2\x1b[0m\n");
     printf("Welcome to the Calcinator 3000\n");
     printf("\x1b[4mSupported Operations\x1b[0m:\n");
     printf("\x1b[31m+ = Addition\x1b[0m\n");
@@ -30,11 +30,11 @@ int main() {
         while (*p == ' ' || *p == '\t') p++;
         if (sscanf(p, "%d", &next_num) == 1) {
             while (*p && (*p >= '0' && *p <= '9')) p++;
-            // Check for factorial or root after number
+
             if (*p == '!') {
                 printf("Factorial of %d is %llu\n", next_num, fakultaet(next_num));
                 p++;
-                first = 1; // reset for next input
+                first = 1;
                 num = 0;
                 continue;
             } else if (*p == 'r') {
