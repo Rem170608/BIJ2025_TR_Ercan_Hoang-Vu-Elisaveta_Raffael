31b3aef8114eb9520355230830608b8c3da5f993
raffa <rem_1706@proton.me>
2025-08-22 13:53:13 +0200

    Final-version

---
+++ Alcohol.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include "headers/animations.h"
+#include "headers/alc.h"
+
+int main() {
+    ASCII_2();
+
+    double weight, alcohol_grams, okay_garmin_video_speichern;
+    char sex;
+
+    printf("Enter your weight (kg): ");
+    scanf("%lf", &weight);
+
+    printf("Enter your sex (M/F/D): ");
+    scanf(" %c", &sex);
+
+    printf("Enter total grams of alcohol consumed: ");
+    scanf("%lf", &alcohol_grams);
+
+    okay_garmin_video_speichern = calculate_bac(weight, sex, alcohol_grams);
+
+    printf("------------------------------------\n");
+    printf("Estimated Blood alc: %.3f ‰\n", okay_garmin_video_speichern);
+    printf("====================================\n");
+
+    return 0;
+}
